12a52f5562a5628818ad61d1e46622551c6cb21c
Yann-cmd <yannguillevic12@gmail.com>
2024-05-24 12:01:59 +0200

    feat: finalisation ceasar

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -5,7 +5,14 @@ context('Hello World!', () => {
     });
 
     it('p should have text Hello World!', () => {
-        cy.get('p').should('have.text', 'Hello World!');
+        cy.get('p').should('have.text', 'Caesar Cypher');
+    });
+
+    it('ceasar compute should be correct', () => {
+        cy.get('#shift').type('6');
+        cy.get('#text').type('Hello World!');
+        cy.get('#Cypher').click()
+        cy.get('#result').should('have.text', 'Nkrru Cuxrj!');
     });
 })
    
